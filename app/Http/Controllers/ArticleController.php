f839d77b7a70866e5661f961cd684d76415f6b4e
Dev205 <dev205mg@gmail.com>
2025-05-30 09:27:59 +0300

    fix request

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ArticleRequest; // request si on ajoute methode store et update
 use App\Models\Article;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -14,7 +14,7 @@ class ArticleController extends Controller
     public function index(): JsonResponse
     {
         $articles = Article::paginate(10);
-        
+
         return response()->json($articles);
     }
 
